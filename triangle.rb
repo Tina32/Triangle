2b6c7b4b82723a0534288e49d6f66f207e15df82
Gatina Foy <foygatina@yahoo.com>
2016-06-20 11:17:54 -0400

    added new test and refactor

--- triangle.rb
+++ triangle.rb
@@ -1,22 +1,34 @@
 
-# calls the method for Triangle
+
 def triangle(a, b, c)
 
-  # checks if triangle is a valid triangle
-  if ( (a+b<=c) || (a+c<=b) || (b+c<=a) )
-    return :inequality
-    # checks if triangle is equilateral
-  elsif ((a == b) && (a == c) && (b == c))
+  if (notValidTriangle(a, b, c))
+    return :notvalid
+  elsif (allSidesEqualTriangle(a,b,c))
     return :equilateral
-    # checks is triangle is isosoceles
-  elsif ((a == b) || (a == c) || (b == c))
+  elsif (twoEqualSides(a,b,c))
     return :isosceles
   else
-    # resturns scalene if triangle has 3 different sides
     return :scalene
   end
 end
 
-# puts triangle(3, 3, 3)
-# puts triangle(3, 4, 5)
+def notValidTriangle(a, b, c)
+  return (a+b<=c) || (a+c<=b) || (b+c<=a)
+end
+
+def allSidesEqualTriangle(a,b,c)
+  return (a == b) && (a == c) && (b == c)
+end
+
+def twoEqualSides(a,b,c)
+  return (a == b) || (a == c) || (b == c)
+end
+
+# def unequalTriangle(a,b,c)
+#   return (a != b) && (a!= c) || (b != c)
+# end
+
+
+
 
--- triangle.spec.rb
+++ triangle.spec.rb
@@ -14,9 +14,18 @@ describe 'triangle' do
     result = triangle(3, 3, 4)
     expect(result).to eq(:isosceles)
   end
-
-    it "should return inequality for a triangle that is not valid" do
+  it "should return inequality for a triangle that is not valid" do
       result = triangle(1, 1, 4)
-      expect(result).to eq(:inequality)
+      expect(result).to eq(:notvalid)
+  end
+  it "should return illegal for 0x0x0 triangle " do
+    result = triangle(0, 0, 0)
+    expect(result).to eq(:notvalid)
+  end
+  it "should return danger for -3x2x4 triangle " do
+    result = triangle(-3, 2, 4,)
+    expect(result).to eq(:notvalid)
   end
 end
+
+
